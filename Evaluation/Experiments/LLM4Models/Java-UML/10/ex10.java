887a2248ce2c8ff8fa126cf190f3a8cf20ada37c
Hanan Siala <k20122072@erc-hpc-comp033.create.kcl.ac.uk>
2025-08-16 17:02:21 +0100

    Added experiment directories from /scratch/users/k20122072/aaa

---
+++ Evaluation/Experiments/LLM4Models/Java-UML/10/ex10.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class PowerUnit { 
+    int power = 430; 
+}
+class Vehicle { 
+    float speed = 3.4f;    
+    float fuelEfficiency = 9.6f;  
+    public PowerUnit calculatePower(int distance) {
+        return new PowerUnit();  
+    }
+}
---
+++ Evaluation/Experiments/LLM4Models/Java-UML/8/ex8.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Engine { 
+  int horsepower = 430;
+}
+class Car { 
+  float speed = 3.4f;
+  float fuelEfficiency = 9.6f;
+  public int calculateFuelEfficiency(Engine engine) {
+     int result;
+      return 39;
+  }
+}
