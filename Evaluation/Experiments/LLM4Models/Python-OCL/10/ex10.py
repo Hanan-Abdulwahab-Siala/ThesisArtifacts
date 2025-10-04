afb6c01238d755be1821775b8f0f2de4784bfe4f
Hanan Siala <k20122072@erc-hpc-comp036.create.kcl.ac.uk>
2025-10-04 17:26:12 +0100

    Added experiment directories from /scratch/users/k20122072/aaa

---
+++ Evaluation/Experiments/LLM4Models/Python-OCL/10/ex10.py
@@ -0,0 +1,5 @@
+num = -54.3
+absolute_value = abs(num)
+print(absolute_value)
+rounded_value = round(num)
+print(rounded_value)
---
+++ Evaluation/Experiments/LLM4Models/Python-OCL/4/ex4.py
@@ -0,0 +1,8 @@
+number = 11  
+if number > 10:
+   result = number * number  
+elif number > 0:
+   result = number * number * number  
+else:
+   result = -number  
+print("Final result:", result)
