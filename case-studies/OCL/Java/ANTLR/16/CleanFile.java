66f2269a5a82dcfa11c9b762fdf4c4b5a0f010b2
HA-Siala <146714014+HA-Siala@users.noreply.github.com>
2025-11-07 09:42:21 +0200

    Delete case-studies directory

--- case-studies/OCL/Java/ANTLR/16/CleanFile.java
+++
@@ -1,9 +0,0 @@
-public class Mapper<T extends ArrayList & Formattable, V> {
-    public void add(T array, V item) {
-        array.add(item);
-        Mapper<CustomList, Integer> mapper = new Mapper<CustomList, Integer>();
-        Mapper<CustomList, ?> mapper;
-        mapper = new Mapper<CustomList, Boolean>();
-        mapper = new Mapper<CustomList, Integer>();
-    }
-}
--- case-studies/UML/Java/2/StudentManagementSystem/ConferenceScoreBehaviour.java
+++
@@ -1,3 +0,0 @@
-public interface ConferenceScoreBehaviour {
-    float conferenceScore();
-}
