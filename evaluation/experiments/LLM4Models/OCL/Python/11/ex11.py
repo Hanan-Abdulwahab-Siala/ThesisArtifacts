63ba675e13938bcbca3cac83ba40f0c6bc0d9324
HA-Siala <146714014+HA-Siala@users.noreply.github.com>
2025-11-26 11:18:47 +0200

    Delete evaluation/experiments/LLM4Models directory

--- evaluation/experiments/LLM4Models/OCL/Python/11/ex11.py
+++
@@ -1,5 +0,0 @@
-text_original = "Hello World"
-text_capitalized = text_original.capitalize()
-text_modified = text_original.replace(" ", "*")
-print(text_capitalized)
-print(text_modified)
--- evaluation/experiments/LLM4Models/OCL/Python/4/ex4.py
+++
@@ -1,8 +0,0 @@
-number = 11  
-if number > 10:
-   result = number * number  
-elif number > 0:
-   result = number * number * number  
-else:
-   result = -number  
-print("Final result:", result)
